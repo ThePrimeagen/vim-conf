beda754054fbeb492521c7cbf301a9d339da28bf
Michael Paulson <mpaulson@netflix.com>
2020-08-21 14:15:33 -0700

    conteuhontheuoenhtuouetnhoeu hntouehnt oeuhn h ntounht uoenhtouenhtntohuenht uenht uenht ueon thueon htueon heuon huoen huoe nhn htuehnt uehtn uenth uonht uon thuen thueon thuen theon tueonhtueonhtueonhtueohtnueon hunoht hnt ounht oenht ounht uenht eunh tuenht uonht iueounhteuonht uenhdt uhtnd uhndt enht uenoahtdhn tuehnt ounh tuon htuinht miunht ueinh tueonh ueonhtouenthd ueonhtd ueonht ueonht ueonht ueonht uoenht ueont ueotn eoun heo hnueonh ueon hueohntueondheoun heouin oiuenhuiedh i huohsiudadhs nsdn haudnshieuadoieuanoiseadosnu dnsdt osnd t;

--- test.py
+++ test.py
@@ -3,29 +3,82 @@ import pandas as pd
 import numpy as np
 
 plt.style.use("dark_background")
-df = pd.read_csv('~/apm.csv', dtype={"TotalTime": float})
 
-totalTime = df["TotalTime"] * 1000     #.plot.hist(bins=10, alpha=0.5)
-totalTime = totalTime.drop(50)
+def plot(df, name):
+    dfNp = np.array(df)
+    mean = np.mean(dfNp)
+    m = np.median(dfNp)
+    std = np.std(dfNp)
+    sweetSweetFrame = df.loc[df < 3000]
 
-insertTime = df["TotalTime"] * 1000     #.plot.hist(bins=10, alpha=0.5)
-for i in range(1, 4):
-    insertTime -= df[f"Stroke{i}"] * 1000
+    print("STD", std)
+    print("mean", mean)
+    print("median", m)
 
-insertTimeNp = np.array(insertTime)
-mean = np.mean(insertTimeNp)
-std = np.std(insertTimeNp)
-sweetSweetFrame = insertTime.loc[(abs(insertTime - mean) < 3 * std)]
-print(std)
+    sweetSweetFrame.plot.hist(bins=100)
+    """
+    Insertion Time time only
+    1   || Datafram length 644
+    1 || Datafram length 970
+    2 || STD 8816.13762502606
+    3 || mean 1225.4209563503427
+    4 || median 289.0841505001149
 
-bins = [std / i for i in range(10, 1, -1)]
-bins.append(std * 0.75)
-bins.append(std)
-bins.append(std * 2)
-bins.append(std * 3)
-sweetSweetFrame.plot.hist(bins=bins)
+    1   || Datafram length 644
+    1 || Datafram length 970
+    2 || STD 8816.13762502606
+    3 || mean 1225.4209563503427
+    4 || median 289.08415050011496
 
-plt.show()
+    NO Insertion Time time only
+
+
+    ThePrimeagen 1127
+    || STD 4092.1784666863027
+    || mean 735.4323119038451
+    || median 261.72505501017
+
+    Teeeeeeej 736
+    || STD 1797.9185813307938
+    || mean 555.4564003767608
+    || median 282.116503
+    """
+
+    plt.title(name)
+    plt.xlim([0, 3000])
+    plt.show()
+
+def getDataSet(path, isinList = False, invertList = False, insertionTimeOnly = True):
+
+    df = pd.read_csv(path, dtype={"TotalTime": float})
+
+    if isinList:
+        inList = df["KeyStrokes"].isin(isinList)
+        if invertList:
+            df = df[~inList]
+        else:
+            df = df[inList]
+
+    totalTime = df["TotalTime"] * 1000     #.plot.hist(bins=10, alpha=0.5)
+
+    if insertionTimeOnly:
+        for i in range(1, 4):
+            totalTime -= df[f"Stroke{i}"] * 1000
+
+    print("Datafram length", len(df))
+    return totalTime
+
+
+isinList = ["o", "O", "i", "I", "a", "A"]
+invertList = False
+insertionTimeOnly = True
+
+tj = getDataSet("~/tj.apm.csv", isinList=isinList, invertList=invertList, insertionTimeOnly=insertionTimeOnly)
+me = getDataSet("~/apm.csv", isinList=isinList, invertList=invertList, insertionTimeOnly=insertionTimeOnly)
+
+frames = [tj, me]
+
+plot(pd.concat(frames, sort=False), "All")
 
 # df["TARGET"] = df.TitalTime.apply(lambda x: "short" if x<0.3 else ("medium" if x<=0.6 else "long"))
 
